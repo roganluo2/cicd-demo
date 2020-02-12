131bbe210fad26bfef584ba7d26bc4bc0369d150
rogan.luo2 <280493748@qq.com>
2020-02-12 23:54:45 +0800

    first commit

---
+++ src/main/java/com/k8s/springboot/cicddemo/CicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.k8s.springboot.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/k8s/springboot/cicddemo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.springboot.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
